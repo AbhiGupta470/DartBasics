ece74d1903442e8e1f8edc952058eb3ad0cac42c
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-07-15 01:29:45 +0530

    Create BridgedRowPullUpsWannaTry.dart

---
+++ ProjectPreparation/UnderWeight/WorkOutScreens/PullUpWannaTries/BridgedRowPullUpsWannaTry.dart
@@ -0,0 +1,120 @@
+// ignore_for_file: prefer_const_constructors, unused_field, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class BridgedRowPullUpsWannaTry extends StatefulWidget {
+  const BridgedRowPullUpsWannaTry({Key? key}) : super(key: key);
+
+  @override
+  State<BridgedRowPullUpsWannaTry> createState() =>
+      _BridgedRowPullUpsWannaTry();
+}
+
+class _BridgedRowPullUpsWannaTry extends State<BridgedRowPullUpsWannaTry>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 10), vsync: this)
+        ..repeat();
+  bool _isVisible = true;
+  //double num  = ;
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+        home: Scaffold(
+            body: SizedBox(
+              width: double.infinity,
+              child: Center(
+                child: Column(mainAxisSize: MainAxisSize.min, children: [
+                  AnimatedBuilder(
+                    animation: _controller,
+                    child: Image(
+                        height: 200,
+                        image: AssetImage('images/FlutterMapps.png')),
+                    builder: (BuildContext context, Widget? child) {
+                      return Transform.rotate(
+                        angle: _controller.value * 50.0 * pi / 12.0,
+                        child: child,
+                      );
+                    },
+                  ),
+                  Visibility(
+                    visible: _isVisible,
+                    child: Container(
+                        height: 230,
+                        width: 350,
+                        alignment: Alignment.bottomLeft,
+                        padding: EdgeInsets.all(40),
+                        color: Colors.orange,
+                        child: ListView(
+                          children: [
+                            Column(children: [
+                              Text(
+                                """Step-1: Position a block or step in front of the TRX straps. Sit down under the handles and grab them. Walk your feet onto the block — you should hovering horizontally over the floor. 
+
+Step-2: With arms in front of your chest, pull your body up to your hands.
+""",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold),
+                              )
+                            ])
+                          ],
+                        )),
+                  ),
+                  AnimatedBuilder(
+                    animation: _controller,
+                    child: Image(
+                        height: 200,
+                        image: AssetImage('images/FlutterMapps.png')),
+                    builder: (BuildContext context, Widget? child) {
+                      return Transform.rotate(
+                        angle: _controller.value * 50.0 * pi / 12.0,
+                        child: child,
+                      );
+                    },
+                  ),
+                ]),
+              ),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.red[100],
+              foregroundColor: Colors.black,
+              splashColor: Colors.blueGrey,
+              tooltip: 'See Above!',
+              onPressed: () {
+                setState(() {
+                  _isVisible = !_isVisible;
+                });
+              },
+              child: Icon(Icons.play_arrow_sharp),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              shape: CircularNotchedRectangle(),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.home,
+                          color: Colors.black54,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.info,
+                          color: Colors.black54,
+                        )),
+                  ]),
+            )));
+  }
+}
