5715c69bd65640db62567f67e7dd4657ae2b7c8d
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2022-07-15 01:10:59 +0530

    Create Hollow_HoldWannaTry.dart

---
+++ ProjectPreparation/UnderWeight/WorkOutScreens/PullUpWannaTries/Hollow_HoldWannaTry.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: prefer_const_constructors, unused_field, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class HollowHoldPullUpsWannaTry extends StatefulWidget {
+  const HollowHoldPullUpsWannaTry({Key? key}) : super(key: key);
+
+  @override
+  State<HollowHoldPullUpsWannaTry> createState() =>
+      _HollowHoldPullUpsWannaTry();
+}
+
+class _HollowHoldPullUpsWannaTry extends State<HollowHoldPullUpsWannaTry>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 10), vsync: this)
+        ..repeat();
+  bool _isVisible = true;
+  //double num  = ;
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+        home: Scaffold(
+            body: SizedBox(
+              width: double.infinity,
+              child: Center(
+                child: Column(mainAxisSize: MainAxisSize.min, children: [
+                  AnimatedBuilder(
+                    animation: _controller,
+                    child: Image(
+                        height: 200,
+                        image: AssetImage('images/FlutterMapps.png')),
+                    builder: (BuildContext context, Widget? child) {
+                      return Transform.rotate(
+                        angle: _controller.value * 50.0 * pi / 12.0,
+                        child: child,
+                      );
+                    },
+                  ),
+                  Visibility(
+                    visible: _isVisible,
+                    child: Container(
+                        height: 230,
+                        width: 350,
+                        alignment: Alignment.bottomLeft,
+                        padding: EdgeInsets.all(40),
+                        color: Colors.orange,
+                        child: ListView(
+                          children: [
+                            Column(children: [
+                              Text(
+                                """Step-1: Lie your back on the floor with your arms extended by your ears. 
+                                
+Step-2: Lift your legs off the ground and your arms overhead simultaneously to hold a hollow position.""",
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold),
+                              )
+                            ])
+                          ],
+                        )),
+                  ),
+                  AnimatedBuilder(
+                    animation: _controller,
+                    child: Image(
+                        height: 200,
+                        image: AssetImage('images/FlutterMapps.png')),
+                    builder: (BuildContext context, Widget? child) {
+                      return Transform.rotate(
+                        angle: _controller.value * 50.0 * pi / 12.0,
+                        child: child,
+                      );
+                    },
+                  ),
+                ]),
+              ),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.red[100],
+              foregroundColor: Colors.black,
+              splashColor: Colors.blueGrey,
+              tooltip: 'See Above!',
+              onPressed: () {
+                setState(() {
+                  _isVisible = !_isVisible;
+                });
+              },
+              child: Icon(Icons.play_arrow_sharp),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              shape: CircularNotchedRectangle(),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.home,
+                          color: Colors.black54,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.info,
+                          color: Colors.black54,
+                        )),
+                  ]),
+            )));
+  }
+}
